a07c2228546d41bbab7b59ac83d8480439412d93
Farel Nouval <fareldazz123@gmail.com>
2023-07-05 17:05:50 +0700

    sdd

--- js/controller/post.js
+++ js/controller/post.js
@@ -3,27 +3,31 @@ import { onClick, getValue } from 'https://bukulapak.github.io/element/process.j
 import { urlPOST, AmbilResponse } from '../config/url_post.js';
 
 function pushData() {
-  var hari_kerja = getValue('hari_kerja');
 
   let data = {
-    longitude: parseFloat(getValue('longitude')),
-    latitude: parseFloat(getValue('latitude')),
-    location: getValue('location'),
-    phone_number: getValue('phone_number'),
-    checkin: getValue('checkin'),
-    biodata: {
-      nama: getValue('nama'),
-      jabatan: getValue('jabatan'),
-      jam_kerja: [
-        {
-          durasi: parseInt(getValue('durasi')),
-          jam_masuk: getValue('jam_masuk'),
-          jam_keluar: getValue('jam_keluar'),
-        },
-      ],
-      hari_kerja: hari_kerja.split(','),
+    orangtua: {
+      nama_ot: getValue('nama_ot'),
+      phone_number: getValue('phone_number'),
+      anak: {
+        Nama: getValue('nama'),
+        NPM: parseInt(getValue('NPM')),
+        Jekel: getValue('Jekel'),
+        phone_number: getValue('phone_number')
+      }
+
     },
-  };
+    tema: {
+      nama_tema: getValue('nama_tema')
+    },
+    dosenwali: {
+      nama_dosen: getValue('nama_dosen'),
+      alamat: getValue('alamat'),
+      phone_number: getValue('phone_number'),
+      email: getValue('email'),
+    },
+    tanggal: getValue('tanggal'),
+    hari: getValue('hari'),
+        }
   postData(urlPOST, data, AmbilResponse);
 }
 
